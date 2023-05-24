1899f1113ce83753ab0ce59933d9ebeb67cdf283
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-05-24 12:19:21 -0400

    seeded with 10 restaurants and 0-5 reviews per restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleanning the db...'
+Restaurant.destroy_all
+Review.destroy_all
+puts 'creating 10 restaurants & 1 to 5 reviews per restaurant'
+
+10.times do
+  resto = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "The restaurant with the id #{resto.id} was created"
+
+  rand(0..5).times do
+    review = Review.create!(
+      restaurant: resto,
+      rating: rand(0..5),
+      content: Faker::Restaurant.review
+    )
+    puts "The review with the id #{review.id} for the restaurant #{resto.name} was created"
+  end
+end
