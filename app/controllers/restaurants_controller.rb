c6339713529693c996bb84d4db2fc139624df9a1
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-05-24 14:27:17 -0400

    new & create for Restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,15 +8,22 @@ class RestaurantsController < ApplicationController
   def show; end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
   end
 
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(%i[name address category])
   end
 
 end
